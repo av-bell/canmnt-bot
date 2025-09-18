21d534fa047781882c020334cdb30973ea69838e
alice <alicebell2713@gmail.com>
2025-09-18 23:47:56 +0000

    Update getPostText.ts

--- src/lib/getPostText.ts
+++ src/lib/getPostText.ts
@@ -77,7 +77,7 @@ export default async function getPostText()
 		contentString = contentString.slice(1,-1); // Remove the quotation marks.
 		contentString = contentString.replace(twitterReg, "").replace(waltRuffReg, "notwaltruff.bsky.social").replace(sportsBotsReg, "").replace(logoReg, "").replace(quoteReg, `"`).replace(andReg, "&").replace(pReg, "\n\n").replace(brReg, "\n").replace(tagReg, ""); //Use the ", &, <p>, and <br> regexes to apply appropriate formatting. Then use the general regex to remove the HTML formatting from the mastodon post. 
 
-		if (contentString.includes("@DarbyAllin") || contentString.includes("@JonMoxley") || contentString.includes("@MercedesVarnado") || contentString.includes("@SussexCoChicken") || contentString.includes("@The305MVP")  || contentString.includes("@KingRicochet")  || contentString.includes("@The305MVP")  || contentString.includes("@The305MVP") || contentString.includes("RETWEET "))
+		if (contentString.includes("@DarbyAllin") || contentString.includes("@JonMoxley") || contentString.includes("@MercedesVarnado") || contentString.includes("@SussexCoChicken") || contentString.includes("@The305MVP")  || contentString.includes("@KingRicochet")  || contentString.includes("@The_MJF")  || contentString.includes("@RatedRCope") || contentString.includes("RETWEET "))
 		{
 			contentString = contentString.replace("@DarbyAllin","Darby Allin")
 			contentString = contentString.replace("@JonMoxley","Jon Moxley");
@@ -85,8 +85,8 @@ export default async function getPostText()
 			contentString = contentString.replace("@SussexCoChicken","Mark Briscoe");
 			contentString = contentString.replace("@The305MVP","MVP");
 			contentString = contentString.replace("@KingRicochet","Ricochet");
-			contentString = contentString.replace("@The305MVP","MVP");
-			contentString = contentString.replace("@The305MVP","MVP");
+			contentString = contentString.replace("@The_MJF","MJF");
+			contentString = contentString.replace("@RatedRCope","Adam Copeland");
 		}
 		
 		if (objJSON[i]["card"] != null)

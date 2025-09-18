ade878049168a3ea4bc5d25817622f685039bded
alice <alicebell2713@gmail.com>
2025-09-18 22:48:34 +0000

    Update getPostText.ts

--- src/lib/getPostText.ts
+++ src/lib/getPostText.ts
@@ -79,7 +79,7 @@ export default async function getPostText()
 
 		if (contentString.includes("@SwerveConfident") || contentString.includes("HarrisTeeter") || contentString.includes("RT ") || contentString.includes("Retweet ") || contentString.includes("retweet ") || contentString.includes("RETWEET "))
 		{
-			contentString = contentString.replace("@SwerveConfident","Swerve Strickland");
+			contentString = contentString.replace("@DarbyAllin","Darby Allin");
 		}
 		
 		if (objJSON[i]["card"] != null)

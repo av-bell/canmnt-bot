26ee675af29341ce1b8210a195e67e9e9db17967
alice <alicebell2713@gmail.com>
2025-10-02 21:26:20 +0000

    Update getPostText.ts

--- src/lib/getPostText.ts
+++ src/lib/getPostText.ts
@@ -614,6 +614,7 @@ export default async function getPostText()
 			contentString = contentString.replace("Happy Birthday! Swerve Strickland","Happy Birthday Swerve Strickland!");
 			contentString = contentString.replace("Happy Birthday! Skye Blue","Happy Birthday Skye Blue!");
 			contentString = contentString.replace("Happy Birthday! Johnny TV","Happy Birthday Johnny TV!");
+			contentString = contentString.replace("Happy Birthday! Emi Sakura","Happy Birthday Emi Sakura!");
 
 			
 		}

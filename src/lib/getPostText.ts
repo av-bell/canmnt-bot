566938166ad95d55e85994e75c7aeb854d762b07
av-bell <alisonbell.ottawa@proton.me>
2025-10-22 14:59:19 +0000

    Update getPostText.ts

--- src/lib/getPostText.ts
+++ src/lib/getPostText.ts
@@ -686,6 +686,7 @@ export default async function getPostText()
 			contentString = contentString.replace("Happy Birthday! @JayWhiteNZ 🎁","Happy Birthday Jay White! 🎁");
 			contentString = contentString.replace("Happy Birthday! Jay White 🎁","Happy Birthday Jay White! 🎁");
 			contentString = contentString.replace("Happy Birthday! Don Callis 🎁","Happy Birthday Don Callis! 🎁");
+			contentString = contentString.replace("Happy Birthday! Taya Valkyrie 🎁","Happy Birthday Taya Valkyrie! 🎁");
 			contentString = contentString.replace("Happy Birthday! Michael Nakazawa 🎁","Happy Birthday Michael Nakazawa! 🎁");
 
 			

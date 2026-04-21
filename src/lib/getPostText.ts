6aff7436a8e9d6a5e6e16eedc889af5603f48f3d
av-bell <alisonbell.ottawa@proton.me>
2026-04-21 15:43:09 -0400

    Update getPostText.ts

--- src/lib/getPostText.ts
+++ src/lib/getPostText.ts
@@ -404,6 +404,8 @@ export default async function getPostText()
 			|| contentString.includes("@HenareNZ")
 			|| contentString.includes("@amisylle")
 			|| contentString.includes("@Kamille_brick")
+			|| contentString.includes("@njpwglobal")
+			|| contentString.includes("@we_are_stardom")
 			|| contentString.includes("Happy Birthday!")
 		    || contentString.includes("Happy Birthday! @DariusMartin612")
 			|| contentString.includes("Happy Birthday! @SNM_Buddy")
@@ -742,6 +744,8 @@ export default async function getPostText()
 			contentString = contentString.replace(".Queen Aminata","Queen Aminata");
 			contentString = contentString.replace(".Henare","Henare");
 			contentString = contentString.replace("@Kamille_brick","Kamille");
+			contentString = contentString.replace("@we_are_stardom","Stardom");
+			contentString = contentString.replace("@njpwglobal","NJPW");
 			contentString = contentString.replace(".Kamille","Kamille");
 			contentString = contentString.replace(".Viva Van","Viva Van");
 			contentString = contentString.replace(".Matt Sydal","Matt Sydal");
